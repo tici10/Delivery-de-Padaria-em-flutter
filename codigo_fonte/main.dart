dcab036f25a0c1d506b3a53239a30420f725d607
Ticiane <114961257+tici10@users.noreply.github.com>
2024-04-10 21:34:53 -0300

    Update main.dart

--- codigo_fonte/main.dart
+++ codigo_fonte/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
       home: Primeiratela(),
     );
   }
