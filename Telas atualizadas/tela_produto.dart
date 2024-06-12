030b1886e2e14e71652c8b521cab63955cb3df64
Ticiane <114961257+tici10@users.noreply.github.com>
2024-06-12 15:27:37 -0300

    Update tela_produto.dart

--- Telas atualizadas/tela_produto.dart
+++ Telas atualizadas/tela_produto.dart
@@ -1,9 +1,12 @@
+import 'dart:collection';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:projeto_ticiane/tela_comprar.dart';
+import 'package:padaria_projeto/tela_comprar.dart';
 
 
-class Produto extends StatelessWidget {
-  const Produto({super.key});
+class Produtos extends StatelessWidget {
+  const Produtos({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,17 +14,30 @@ class Produto extends StatelessWidget {
       appBar: AppBar(
         title: Text('Produto'),
       ),
-      body:ButtonTheme(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const Comprar()),
-            );
-          },
-        child: Text('Entrar'), 
-        )  
-      )
+      body:Column(
+        children:<Widget>[
+          Title(
+            color: Colors.black, 
+            child: Text('Pão de Queijo')),
+          SizedBox(
+            width: 200,
+            height: 200,
+            child:Image.asset('assets/images/pao_de_queijo.jpg'),
+          ),
+          const Text('Pão de queijo feito com farinha de mandioca, queijo, leite e ovo'),
+          ButtonTheme(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Comprar()),
+                );
+              },
+            child: Text('Entrar'), 
+            )  
+          )
+        ]
+      ),
     );
   }
 }
