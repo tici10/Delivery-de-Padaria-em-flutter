1e05daa1ad0a9f3036fd262025dd3a307c44be33
Ticiane <114961257+tici10@users.noreply.github.com>
2024-06-19 15:59:42 -0300

    Add files via upload

---
+++ Telas atualizadas/produto.dart
@@ -0,0 +1,15 @@
+class Produto {
+  int id;
+  String nome;
+  double preco;
+  String descricao;
+  String imagem;
+
+  Produto({
+    required this.id,
+    required this.nome,
+    required this.preco,
+    required this.descricao,
+    required this.imagem,
+  });
+}
